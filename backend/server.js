9a39a1161b9c955f16c6cdb85a31369f24dc3a68
daviiess <daviestory1@gmail.com>
2026-05-20 16:26:31 +0100

    included json middleware

--- backend/server.js
+++ backend/server.js
@@ -30,6 +30,10 @@ app.use(cors({
   credentials: true
 }));
 
+app.use('/uploads', express.static(path.join(__dirname, 'uploads')));
+app.use(express.json());
+app.use(express.urlencoded({extended: true}));
+
 
 
 //Routes
